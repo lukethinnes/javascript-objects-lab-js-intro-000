4edccdf299375e7f5e36f289a8df3f46e31b0875
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-19 19:37:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var recipes = {eggs: '3', cheese: '1/4 cup', milk: '1/2 cup'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
+  console.log(recipes)
   return Object.assign({}, key, value)
+  recipes
 }
